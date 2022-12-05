59b7c634559b005bb4a0d3d34f22b50c2d8c1533
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2022-12-05 12:48:37 +0800

    Добавьте файлы проекта.

---
+++ little bits drive me crazy/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace little_bits_drive_me_crazy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DisPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DisPic
+            // 
+            this.DisPic.Location = new System.Drawing.Point(0, 1);
+            this.DisPic.Name = "DisPic";
+            this.DisPic.Size = new System.Drawing.Size(940, 465);
+            this.DisPic.TabIndex = 0;
+            this.DisPic.TabStop = false;
+            this.DisPic.Click += new System.EventHandler(this.DisPic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 467);
+            this.Controls.Add(this.DisPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DisPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DisPic;
+    }
+}
+
---
+++ little bits drive me crazy/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace little_bits_drive_me_crazy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisPic.Image = new Bitmap(DisPic.Width, DisPic.Height);
+        }
+
+        private void DisPic_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
